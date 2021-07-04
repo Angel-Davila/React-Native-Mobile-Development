02bbd723cfd0d3bc30267de4993bad8e5625b02b
Angel-Davila <a6519150005@utch.edu.mx>
2021-07-04 11:48:30 -0600

    listeners and handlers added

--- src/components/BadgesScreen/BadgesScreen.js
+++ src/components/BadgesScreen/BadgesScreen.js
@@ -1,46 +1,138 @@
 import React from 'react';
-import { View, ActivityIndicator, StyleSheet, FlatList, Text } from 'react-native';
-import Colors from '../../res/Colors';
-import Http from '../../libs/http';
+import {
+    View,
+    ActivityIndicator,
+    StyleSheet,
+    FlatList,
+    StatusBar,
+    Text,
+    Alert,} from 'react-native'
+import BadgesItem from './BadgesItem';
+import Colors from '../../res/Colors'
+import Http from '../../libs/http'
+import BadgesSearch from './BadgesSearch';
 
-class BadgesScreen extends React.Component{
+class BadgesScreen extends React.Component {
     state = {
         loading: false,
-        badges: [],
+        badges: undefined,
+        badgesCopy: undefined,
     };
 
-    componentDidMount(){
+    componentDidMount() {
         this.fetchdata();
+        this.focusEvent();
+        this.blurEvent();
     }
 
-    fetchdata = async() => {
+    focusEvent = () => {
+        this.focusListener = this.props.navigation.addListener('focus', () =>{
+            this.setFetchInterval();
+        });
+        
+    }
+
+    blurEvent = () => {
+        this.blurListener = this.props.navigation.addListener('blur', () =>{
+            clearInterval(this.interval);
+        });
+    }
+
+    setFetchInterval = () =>{
+        this.interval = setInterval(this.fetchdata,3000);
+    };
+
+    fetchdata = async () => {
         this.setState({loading: true});
         let response = await Http.instance.get_all();
-        response = response.reverse();
-        console.log(response);
-        this.setState({loading: false, badges: response});
-
+        this.setState({loading: false, badges: response, badgesCopy: response});
     };
+
+    handlePress = item =>{
+        this.props.navigation.navigate('BadgesDetail', {item});
+    };
+
+    handleEdit = item => {
+        this.props.navigation.navigate('BadgesEdit', {item});
+    }
+
+    handleChange = query => {
+        const {badgesCopy} = this.state;
+
+        const badgesFiltered = badgesCopy.filter(badge => {
+            return badge.name.toLowerCase().includes(query.toLowerCase());
+        });
+
+        this.setState({badges: badgesFiltered});
+
+        if(query){
+            clearInterval(this.interval);
+        }else{
+            this.setFetchInterval();
+        }
+    };
+
+    handleDelete = item => {
+        Alert.alert('Are you sure?',
+        `Do you really want to delete ${item.name}'s badge?\n\nThis process cannot be undone`,
+        
+        [
+            {
+                text:'Cancel',
+                style: 'cancel',
+            },
+            {
+                text: 'Delete',
+                onPress: async () =>{
+                    this.setState({loading:true, badges:undefined});
+                    await Http.instance.remove(item._id);
+                    this.fetchdata();
+                },
+                style:'destructive',
+            },
+        ],
+        {
+            cancelable: true,
+        },
+        );
+    }
+
+    componentWillUnMount () {
+        this.focusListener();
+        this.blurListener();
+    }
 
     render() {
         const {badges, loading} = this.state;
-
-        return ( 
-            <View style={[styles.container, styles.horizontal]}>
-                { loading ? (
+        
+        if(loading===true && !badges){
+            return(
+                <View style={[styles.container, styles.horizontal]}>               
                     <ActivityIndicator
                         style={styles.loader}
-                        color="#F57AEC"
+                        color="#E3198F80"
                         size="large"
                     />
-                ) : null}
-                <FlatList
-                style={styles.list} 
-                data={badges}
-                renderItem={(item) => {
-                <Text>{item.name}</Text>;
-            }}
-                />            
+                </View>
+            );
+        }
+        return (
+            <View style={[styles.container, styles.horizontal]}>
+                <StatusBar backgroundColor="transparent" translucent={true} />
+                <BadgesSearch onChange={this.handleChange}/>
+                <FlatList 
+                    style={styles.list} 
+                    data={badges} 
+                    renderItem={({item})=> 
+                    <BadgesItem 
+                    key={item._id} 
+                    item={item}
+                    onPress={() => this.handlePress(item)}
+                    onEdit={() => this.handleEdit(item)}
+                    onDelete={() => this.handleDelete(item)}
+                    />}
+                    keyExtractor={(item, index) => index.toString()}
+                />
             </View>
         );
     }
@@ -48,18 +140,22 @@ class BadgesScreen extends React.Component{
 
 const styles = StyleSheet.create({
     container:{
-        flex:1,
-        flexDirection:'column',
+        flex: 1,
+        flexDirection: 'column',
         backgroundColor: Colors.charade,
     },
-    horizontal:{
-        justifyContent:'space-around',
+    horizontal: {
+        justifyContent: 'space-around',
         alignItems: 'center',
     },
     loader:{
-        height: '100%',
+        height:'100%',
         paddingHorizontal: 10,
-        },
+    },
+    list:{
+        width: '100%',
+        paddingHorizontal: 10
+    },
 });
 
-export default BadgesScreen; 
+export default BadgesScreen;
